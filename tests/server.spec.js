9ff57e76bd0bba6c19a805bd51e765e42a311b9b
Carolina Lunas <carolinalunasfarah@gmail.com>
2024-02-01 12:27:20 -0300

    create tests for GET /coffees

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -7,5 +7,11 @@ describe("Coffees CRUD operations", () => {
             const response = await request(app).get("/coffees").send();
             expect(response.status).toBe(200);
         });
+
+        it("Should respond with an array and at least 1 object", async () => {
+            const response = await request(app).get("/coffees");
+            expect(Array.isArray(response.body)).toBe(true);
+            expect(response.body[0]).toBeInstanceOf(Object);
+        });
     });
 });
