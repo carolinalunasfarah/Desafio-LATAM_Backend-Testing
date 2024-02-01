c72bd79e69263e8fd7595a5222b3df9442f3a0e5
Carolina Lunas <carolinalunasfarah@gmail.com>
2024-02-01 13:17:16 -0300

    create tests for GET /coffees/:id

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,5 +1,6 @@
 import request from "supertest";
 import app from "../server.js";
+import { faker } from "@faker-js/faker";
 
 describe("Coffees CRUD operations", () => {
     describe("GET /coffees", () => {
@@ -12,6 +13,19 @@ describe("Coffees CRUD operations", () => {
             const response = await request(app).get("/coffees");
             expect(Array.isArray(response.body)).toBe(true);
             expect(response.body[0]).toBeInstanceOf(Object);
+        });
+    });
+
+    describe("GET /coffees/:id", () => {
+        it("Should respond with a 200 code status when using a valid id", async () => {
+            const response = await request(app).get("/coffees/1").send();
+            expect(response.status).toBe(200);
+        });
+
+        it("Should respond with a 404 code status when using an invalid id", async () => {
+            const id = faker.string.alphanumeric();
+            const response = await request(app).get(`/coffees/${id}`);
+            expect(response.status).toBe(404);
         });
     });
 });
